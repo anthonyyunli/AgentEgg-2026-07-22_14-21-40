576a3e3934b7d86e81cfd185bbea08955e79454b
MAbubakarI <Abustar421@gmail.com>
2026-07-26 03:27:40 -0600

    added way to wint he game

---
+++ Assets/Scripts/WinScript.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class WinScript : MonoBehaviour
+{
+
+    public Transform gate;
+
+    [SerializeField] private LayerMask PlayerMask;
+    [SerializeField] private LayerMask BoilEggMask;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        isWin();
+
+    }
+
+    public void isWin()
+    {
+        Vector3 position = gate.position;
+
+         if (Physics.CheckSphere(position,10f,PlayerMask,QueryTriggerInteraction.Ignore) == true && Physics.CheckSphere(position, 10f, BoilEggMask, QueryTriggerInteraction.Ignore) == true)
+        {
+
+            Debug.Log("Game won");
+
+        }
+
+
+    }
+
+
+}
